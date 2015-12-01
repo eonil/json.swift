cd71c8fdcedffc9be94196233494e2b4bb33433a
Hoon H <drawtree@me.com>
2015-12-01 23:09:04 +0900

    Imported from [`Monolith.Swift`](https://github.com/Eonil/Monolith.Swift).

---
+++ EonilJSON/EonilJSON.h
@@ -0,0 +1,19 @@
+//
+//  EonilJSON.h
+//  EonilJSON
+//
+//  Created by Hoon H. on 2015/12/01.
+//  Copyright © 2015 Eonil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EonilJSON.
+FOUNDATION_EXPORT double EonilJSONVersionNumber;
+
+//! Project version string for EonilJSON.
+FOUNDATION_EXPORT const unsigned char EonilJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EonilJSON/PublicHeader.h>
+
+
